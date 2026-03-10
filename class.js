4d99e80fcdf5279c0ec5763d96929a1e918bec63
zainsajjad903 <zainsajjad.903@gmail.com>
2026-03-10 11:45:49 +0500

    first commit

--- class.js
+++ class.js
@@ -1,16 +1,19 @@
 class parent {
-  test = "hello father";
+  test_1 = "hello father";
 }
 class mother {
-  test = "hello mother";
+  test_2 = "hello mother";
+}
+class chacha {
+  test_3 = "hello chacha";
 }
 class child extends mother {
-  test = "hello child";
+  test_2 = "hello child";
 }
 class child2 extends parent {
-  test = "hello child2";
+  test_1 = "hello child2";
 }
 let obj = new child();
-console.log(obj.test);
+console.log(obj.test_2);
 let obj2 = new child2();
-console.log(obj2.test);
+console.log(obj2.test_1);
