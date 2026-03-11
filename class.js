e15b18b06883f90b472e5bf066ae5b1e12050a02
zainsajjad903 <zainsajjad.903@gmail.com>
2026-03-11 10:56:29 +0500

    Updated design and fixed bugs

--- class.js
+++ class.js
@@ -1,22 +1,24 @@
-class parent {
-  test_1 = "hello father";
-}
-class mother {
-  test_2 = "hello mother";
-}
-class chacha {
-  test_3 = "hello chacha";
-}
-class child extends mother {
-  test_2 = "hello child";
-}
-class child2 extends parent {
-  test_1 = "hello child2";
-}
-class child3 extends chacha {
-  test_3 = "hello child3";
-}
-let obj = new child();
-console.log(obj.test_2);
-let obj2 = new child2();
-console.log(obj2.test_1);
+// class parent {
+//   test_1 = "hello father";
+// }
+// class mother {
+//   test_2 = "hello mother";
+// }
+// class chacha {
+//   test_3 = "hello chacha";
+// }
+// class child extends mother {
+//   test_2 = "hello child";
+// }
+// class child2 extends parent {
+//   test_1 = "hello child2";
+// }
+// class child3 extends chacha {
+//   test_3 = "hello child3";
+// }
+// let obj = new child();
+// console.log(obj.test_2);
+// let obj2 = new child2();
+// console.log(obj2.test_1);
+// let obj3 = new child3();
+// console.log(obj3.test_3);
